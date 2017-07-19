8a69aa52397c24968b4a457da440b331b3242aef
Yahya Gilany <yahya.gilany@live.com>
2017-07-19 18:25:49 -0400

    changes to the database file to work with mysql

--- src/libs/database.js
+++ src/libs/database.js
@@ -1,4 +1,4 @@
-const config = require(`../../../config.json`);
+const config = require(`../../config.json`);
 const knex = require(`knex`)({
   client: config.database.dialect,
   connection: {
@@ -7,15 +7,11 @@ const knex = require(`knex`)({
     user     : config.database.username,
     password : config.database.password,
     database : config.database.name,
-    domain: config.database.domain,
-    instanceName: config.database.instanceName,
     charset  : `utf8`
   }
 });
 
 const bookshelf = require(`bookshelf`)(knex);
-
-
 
 exports.__bookshelf = bookshelf;
 exports.jsonify = (ele) => { return ele.toJSON(); };
