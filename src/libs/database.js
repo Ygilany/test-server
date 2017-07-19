49734095b219c9d925316da705a26f649c061c07
Yahya Gilany <yahya.gilany@live.com>
2017-07-19 18:07:16 -0400

    database connection setup

---
+++ src/libs/database.js
@@ -0,0 +1,21 @@
+const config = require(`../../../config.json`);
+const knex = require(`knex`)({
+  client: config.database.dialect,
+  connection: {
+    host     : config.database.host,
+    port     : config.database.port,
+    user     : config.database.username,
+    password : config.database.password,
+    database : config.database.name,
+    domain: config.database.domain,
+    instanceName: config.database.instanceName,
+    charset  : `utf8`
+  }
+});
+
+const bookshelf = require(`bookshelf`)(knex);
+
+
+
+exports.__bookshelf = bookshelf;
+exports.jsonify = (ele) => { return ele.toJSON(); };
